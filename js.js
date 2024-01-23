b6d07c7fc3cd2d87e3f9cf3fba1f342e5e25499c
Izat18 <izatmuktarbekuulu26@gmail.com>
2024-01-23 16:39:40 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector('.burger')
+let burgerBtn = document.querySelector('.burger__btn')
+let toggleBurger = () =>{
+    burger.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+}
